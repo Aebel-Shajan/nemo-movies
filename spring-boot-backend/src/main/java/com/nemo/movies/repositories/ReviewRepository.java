9ffb5e1df917cefc1a2ad2164a42b9414979bec8
YNegash <n.yeabsira.a@gmail.com>
2024-06-27 15:54:07 +0100

    adds repository annotation in ReviewRepository

--- spring-boot-backend/src/main/java/com/nemo/movies/repositories/ReviewRepository.java
+++ spring-boot-backend/src/main/java/com/nemo/movies/repositories/ReviewRepository.java
@@ -3,6 +3,8 @@ package com.nemo.movies.repositories;
 import com.nemo.movies.models.Review;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
