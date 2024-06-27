40f228a350e58dfcbdd5936c96d1c2be09d769cb
YNegash <n.yeabsira.a@gmail.com>
2024-06-27 15:13:37 +0100

    creates review model

---
+++ spring-boot-backend/src/main/java/com/nemo/movies/Review.java
@@ -0,0 +1,26 @@
+package com.nemo.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+
+    @Id
+    private ObjectId id;
+
+    private String body;
+
+//    LocalDateTime created;
+//
+//    LocalDateTime updated;
+}
