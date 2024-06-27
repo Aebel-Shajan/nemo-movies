9ff6316b7416bc727b2d2dfa478555b6b63480a1
YNegash <n.yeabsira.a@gmail.com>
2024-06-27 16:07:22 +0100

    adds create Review method in service

--- spring-boot-backend/src/main/java/com/nemo/movies/services/ReviewService.java
+++ spring-boot-backend/src/main/java/com/nemo/movies/services/ReviewService.java
@@ -2,8 +2,11 @@ package com.nemo.movies.services;
 
 import com.nemo.movies.models.Review;
 import com.nemo.movies.repositories.ReviewRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewService {
@@ -11,5 +14,7 @@ public class ReviewService {
     @Autowired
     ReviewRepository reviewRepository;
 
-
+    public Review createReview(String reviewBody, String imbdId) {
+        Review review = new Review();
+    }
 }
