50a100148c22c709842633c7d76d98f96f747af4
YNegash <n.yeabsira.a@gmail.com>
2024-06-27 15:57:09 +0100

    adds service annotation to ReviewService and autowires review repository into service

--- spring-boot-backend/src/main/java/com/nemo/movies/services/ReviewService.java
+++ spring-boot-backend/src/main/java/com/nemo/movies/services/ReviewService.java
@@ -1,2 +1,15 @@
-package com.nemo.movies.services;public class ReviewService {
+package com.nemo.movies.services;
+
+import com.nemo.movies.models.Review;
+import com.nemo.movies.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+
 }
