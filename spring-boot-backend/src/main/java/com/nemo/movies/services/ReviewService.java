7ba79bec6f8d408f6f27cd398da4b41856c3c695
YNegash <n.yeabsira.a@gmail.com>
2024-06-28 11:45:38 +0100

    completes create review method in review service

--- spring-boot-backend/src/main/java/com/nemo/movies/services/ReviewService.java
+++ spring-boot-backend/src/main/java/com/nemo/movies/services/ReviewService.java
@@ -3,6 +3,9 @@ package com.nemo.movies.services;
 import com.nemo.movies.models.Review;
 import com.nemo.movies.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 
@@ -12,7 +15,18 @@ public class ReviewService {
     @Autowired
     ReviewRepository reviewRepository;
 
-    public void createReview(String reviewBody, String imbdId) {
-       Review review = new Review();
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imbdId) {
+       Review review = reviewRepository.insert(new Review(reviewBody));
+
+       mongoTemplate.update(Movie.class)
+               .matching(Criteria.where("imbdId").is(imbdId))
+               .apply(new Update().push("reviewIds").value(review))
+               .first();
+
+       return review;
     }
+
 }
