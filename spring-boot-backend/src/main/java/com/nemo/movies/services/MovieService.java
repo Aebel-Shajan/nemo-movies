85834be5bb23114c61ed057c15b173afb2eddf63
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-06-28 10:58:01 +0100

    Add movie controller and service

---
+++ spring-boot-backend/src/main/java/com/nemo/movies/services/MovieService.java
@@ -0,0 +1,20 @@
+package com.nemo.movies.services;
+
+import com.nemo.movies.models.Movie;
+import com.nemo.movies.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    @Autowired // tells framework to instantiate variable for us.
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+}
