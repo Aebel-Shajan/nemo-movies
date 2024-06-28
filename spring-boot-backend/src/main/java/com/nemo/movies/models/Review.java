d025e29e4819f6a5be305f1b74053ea5aa6aa052
YNegash <n.yeabsira.a@gmail.com>
2024-06-28 13:13:24 +0100

    completes review model with created and updated properties

--- spring-boot-backend/src/main/java/com/nemo/movies/models/Review.java
+++ spring-boot-backend/src/main/java/com/nemo/movies/models/Review.java
@@ -7,6 +7,8 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Document(collection = "reviews")
 @Data
 @AllArgsConstructor
@@ -15,10 +17,14 @@ public class Review {
 
     @Id
     private ObjectId id;
-
     private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 
-    public Review(String body) {
+    public Review(String body, LocalDateTime created, LocalDateTime updated) {
         this.body = body;
+        this.created = created;
+        this.updated = updated;
     }
+
 }
