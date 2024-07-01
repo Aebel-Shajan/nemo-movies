fd89c871929e6e0cca64bdc01359001d5f9edf36
YNegash <n.yeabsira.a@gmail.com>
2024-07-01 15:13:46 +0100

    adds Id annotation to id property in review model

--- spring-boot-backend/src/main/java/com/nemo/movies/models/Review.java
+++ spring-boot-backend/src/main/java/com/nemo/movies/models/Review.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Review {
 
+    @Id
     private ObjectId id;
     private String body;
     private LocalDateTime created;
