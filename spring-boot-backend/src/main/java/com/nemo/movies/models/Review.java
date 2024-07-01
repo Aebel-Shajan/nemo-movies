9b0db533689e7fb6a6f2f7b3c281394bcad740d8
YNegash <n.yeabsira.a@gmail.com>
2024-07-01 15:11:28 +0100

    removes redundant import

--- spring-boot-backend/src/main/java/com/nemo/movies/models/Review.java
+++ spring-boot-backend/src/main/java/com/nemo/movies/models/Review.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
