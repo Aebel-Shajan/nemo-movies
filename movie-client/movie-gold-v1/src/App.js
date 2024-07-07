1d7b0fe64cb3014b36732b755ee89aeee7a242f9
Y-Negash <n.yeabsira.a@gmail.com>
2024-07-07 18:36:21 +0100

    adds import statements and usestate for movies

--- movie-client/movie-gold-v1/src/App.js
+++ movie-client/movie-gold-v1/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 
 function App() {
+  
+  const[movies, setMovies] = useState();
+  
+  
   return (
     <div className="App">
 
