1eddf358bcbd3b0a1f247c2974b19d01aa2f7108
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-07-07 15:39:18 +0100

    Add placeholder movies list (to be replaced with get request from backend)

--- movie-client/movie-gold-v1/src/App.js
+++ movie-client/movie-gold-v1/src/App.js
@@ -5,12 +5,14 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/home/Home';
 
 function App() {
+  const movies = getPlaceholderMovies()
+
   return (
     <div className="App">
       
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home/>}></Route>
+          <Route path="/" element={<Home movies={movies}/>}></Route>
         </Route>
       </Routes>
 
@@ -19,3 +21,20 @@ function App() {
 }
 
 export default App;
+
+function getPlaceholderMovies() {
+  return [
+    {
+      "title": "Avatar: The Way of Water",
+      "poster": "https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+    },
+    {
+      "title": "Roald Dahl's Matilda the Musical",
+      "poster": "https://image.tmdb.org/t/p/w500/ga8R3OiOMMgSvZ4cOj8x7prUNYZ.jpg"
+    },
+    {
+      "title": "Black Panther: Wakanda Forever",
+      "poster": "https://image.tmdb.org/t/p/w500/cryEN3sWlgB2wTzcUNVtDGI8bkM.jpg"
+    }
+  ]
+}
