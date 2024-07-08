454dff584faba0efe4e32f7c5356f7ac669e707f
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-07-08 15:00:33 +0100

    Fix #9 Error in hero component when initially loading page.

--- movie-client/movie-gold-v1/src/App.js
+++ movie-client/movie-gold-v1/src/App.js
@@ -6,7 +6,7 @@ import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
 
 function App() {
-  const[movies, setMovies] = useState();
+  const[movies, setMovies] = useState([]);
   
   const getMovies = async () => {
     try { 
