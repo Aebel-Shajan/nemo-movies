c47c0d0c36f64e2b2f2eee81a8226e70e0992890
Y-Negash <n.yeabsira.a@gmail.com>
2024-07-08 17:43:11 +0100

    fixes Button import statement

--- movie-client/movie-gold-v1/src/components/header/Header.js
+++ movie-client/movie-gold-v1/src/components/header/Header.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
