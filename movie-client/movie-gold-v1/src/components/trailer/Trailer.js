260cb5d2c08950f94b1567256277be9b4d1e87b6
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-07-08 21:20:55 +0100

    Create Trailer component

---
+++ movie-client/movie-gold-v1/src/components/trailer/Trailer.js
@@ -0,0 +1,27 @@
+import { useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import './Trailer.css';
+
+const Trailer = () => {
+    
+    let params = useParams();
+    let key = params.ytTrailerId;
+
+    return (
+        <div id='react-player-container'>
+            {
+                (key!=null) ? 
+                <ReactPlayer 
+                    controls="true" 
+                    playing={true} 
+                    url={`https://www.youtube.com/watch?v=${key}`}
+                    width='100%'
+                    height='100%'
+                />
+                : null
+            }
+        </div>
+    )
+}
+
+export default Trailer;
