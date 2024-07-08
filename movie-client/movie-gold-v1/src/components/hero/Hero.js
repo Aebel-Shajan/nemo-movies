f53e13a91b9c81f9d46417e632694c753e443494
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-07-08 21:58:22 +0100

    Link play button to trailer endpoint.

--- movie-client/movie-gold-v1/src/components/hero/Hero.js
+++ movie-client/movie-gold-v1/src/components/hero/Hero.js
@@ -4,6 +4,7 @@ import Carousel from 'react-material-ui-carousel'
 import { Paper } from '@mui/material'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import { Link } from "react-router-dom"
 
 const Hero = ({movies}) => {
     return (
@@ -23,12 +24,14 @@ const Hero = ({movies}) => {
                                                 <h4>{movie.title}</h4>
                                             </div>
                                             <div className='movie-buttons-container'>
-                                                <div className='play-button-icon-container'>
-                                                    <FontAwesomeIcon 
-                                                        className='play-button-icon'
-                                                        icon={faCirclePlay}
-                                                    />
-                                                </div>
+                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                    <div className='play-button-icon-container'>
+                                                        <FontAwesomeIcon 
+                                                            className='play-button-icon'
+                                                            icon={faCirclePlay}
+                                                        />
+                                                    </div>
+                                                </Link>
                                             </div>
                                         </div>
                                     </div>
