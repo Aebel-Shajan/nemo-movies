6976e7cca0b94dedce0924a6a6ad2dd5f956b3d0
Y-Negash <n.yeabsira.a@gmail.com>
2024-07-08 17:40:46 +0100

    imports Header into App.js

--- movie-client/movie-gold-v1/src/App.js
+++ movie-client/movie-gold-v1/src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/home/Home';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Header from './components/header/Header';
 
 function App() {
   const[movies, setMovies] = useState([]);
@@ -25,6 +26,7 @@ function App() {
   
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}></Route>
